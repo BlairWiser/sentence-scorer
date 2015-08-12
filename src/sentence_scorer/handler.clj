2dee8473167aed92b9d26b159be645c826442f19
BlairWiser <blair.m.wiser@gmail.com>
2015-08-12 15:20:08 -0400

    Changed get-file-lines in handler.clj to also split on exlamation marks and question marks.

--- src/sentence_scorer/handler.clj
+++ src/sentence_scorer/handler.clj
@@ -26,7 +26,7 @@
   [filename]
   (let [content (slurp filename)]
     ;;Split on commas and periods followed by whitespace
-    (str/split content #",\s|\.\s+"))) 
+    (str/split content #",\s|\.\s+|!\s|?\s"))) 
 
 (defn score-file
   "Takes a filename and returns the scores for the sentences"
@@ -34,28 +34,8 @@
   (let [lines (get-file-lines filename)]
     (vector lines (map score-sentence lines))))
 
-(defn get-sentence-score
-  "Returns n-gram scores of sentence for n 1 through 5"
-  [sentence]  
-  (let [words (tokenize sentence)
-        blm1 (score-by-blm {:words words :N 1 :lm lm})
-        blm2 (score-by-blm {:words words :N 2 :lm lm})
-        blm3 (score-by-blm {:words words :N 3 :lm lm})
-        blm4 (score-by-blm {:words words :N 4 :lm lm})
-        blm5 (score-by-blm {:words words :N 5 :lm lm})]
-    {:status 200
-     :body {:words words
-            :blm_1 blm1
-            :blm_2 blm2
-            :blm_3 blm3
-            :blm_4 blm4
-            :blm_5 blm5}}))
-
 (defroutes app-routes
   ;;(route/files "/" {:root root})
-  (POST "/score/" request
-       (let [sentence (param request :sentence)]
-         (get-sentence-score sentence)))
   (GET "/score/:collectionID/:fileID" [collectionID fileID]
        (let [filename (str root "/" collectionID "/" fileID)
              result (score-file filename)
