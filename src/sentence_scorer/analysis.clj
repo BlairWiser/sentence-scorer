8eab7839c4cd608e9a653e33cdb06dda9da54215
BlairWiser <blair.m.wiser@gmail.com>
2015-08-17 15:08:30 -0400

    Added catching of NaN in generating mean score.

--- src/sentence_scorer/analysis.clj
+++ src/sentence_scorer/analysis.clj
@@ -1,5 +1,6 @@
 (ns sentence-scorer.analysis
-  (:use [sentence-scorer.evaluate]))
+  (:use [sentence-scorer.evaluate])
+  (:require [clojure.string :as str]))
 
 (defn get-weighted-mean
   "Takes a list and returns the mean"
@@ -7,13 +8,15 @@
   (let [k (count scores)
         W (repeat k 1)
         terms (for [i (range k) :let [Wi (nth W i)]]
-                (* Wi (nth scores i)))]
+                (if (Double/isNaN (nth scores i))
+                  (* Wi -100)
+                  (* Wi (nth scores i))))]
     (/ (apply + terms) k)))
 
 (defn get-n-grams
   "Takes a sentence and returns its n-gram scores for n = 5 through 1"
   [lm sentence]
-  (let [words (tokenize sentence)]
+  (let [words (str/split sentence #"\s")]
     (for [i (range 5 0 -1)]
       (:spanScores (score-by-blm {:words words :N i :lm lm})))))
 
